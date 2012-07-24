48e4f741993f386a056b8c79d427a0f385209099
faust45 <faust451@gmail.com>
2012-07-24 21:01:48 +0300

    add ajax post form data

--- www/app.js
+++ www/app.js
@@ -1,3 +1,50 @@
 window.onload = function() {
-    alert(1);
+    var xhr = createRequestObject();
+    //xhr.upload.onprogress  = onprogress;
+    xhr.onreadystatechange = onreadystatechange;
+
+    run();
+
+    function run() {
+        var url = "/"; 
+        var formData = new FormData();
+        formData.append("uploadfile", "data");
+
+        xhr.open("POST", url, true);
+        xhr.send(formData);
+    }
+
+    function onComplete() {
+        var response;
+
+        if (xhr.status == 201 || xhr.status == 200) {
+            try { 
+               response = eval("(" + xhr.responseText + ")"); 
+            }
+            catch(err) { response = {}; }
+
+            alert(response);
+        }
+    }
+
+    function onreadystatechange() {
+        if (xhr.readyState == 4) { onComplete(); }
+    }
 }
+
+function createRequestObject() {
+    var request = null;
+
+    if (!request) try {
+        request = new ActiveXObject('Msxml2.XMLHTTP');
+    } catch (e) {}
+    if (!request) try {
+        request = new ActiveXObject('Microsoft.XMLHTTP');
+    } catch (e) {}
+    if (!request) try {
+        request = new XMLHttpRequest();
+    } catch (e) {}
+
+    return request;
+}
+
